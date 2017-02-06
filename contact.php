65b13a91479bb32e8e8fe1087d52e2c38cae17dd
shocknawe <jairus.aragon@gmail.com>
2017-02-06 12:00:37 +0800

    starting more pages

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php include_once('header.php'); ?>
+
+
+
+<!--
+   ____  _____ _____ _   _   _ _   _____   ____    _    _   _ _   _ _____ ____
+  |  _ \| ____|  ___/ \ | | | | | |_   _| | __ )  / \  | \ | | \ | | ____|  _ \
+  | | | |  _| | |_ / _ \| | | | |   | |   |  _ \ / _ \ |  \| |  \| |  _| | |_) |
+  | |_| | |___|  _/ ___ \ |_| | |___| |   | |_) / ___ \| |\  | |\  | |___|  _ <
+  |____/|_____|_|/_/   \_\___/|_____|_|   |____/_/   \_\_| \_|_| \_|_____|_| \_\
+
+-->
+
+
+<article id="default-page-banner-section">
+  <div id="default-page-banner">
+    <div class="manic-image-container">
+      <img src="" data-image-desktop="images/about-banner.png">
+    </div>
+  </div>
+</article>
+
+
+
+<article id="insertpagehere-insertsectionnamehere-section">
+  <div class="container-fluid has-breakpoint">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="default-copy">
+          <h1>Sample Content</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</article>
+
+
+<?php include_once('footer.php'); ?>
---
+++ privacy.php
@@ -0,0 +1,103 @@
+<?php include_once('header.php'); ?>
+
+<!--
+   ____  _____ _____ _   _   _ _   _____   ____    _    _   _ _   _ _____ ____
+  |  _ \| ____|  ___/ \ | | | | | |_   _| | __ )  / \  | \ | | \ | | ____|  _ \
+  | | | |  _| | |_ / _ \| | | | |   | |   |  _ \ / _ \ |  \| |  \| |  _| | |_) |
+  | |_| | |___|  _/ ___ \ |_| | |___| |   | |_) / ___ \| |\  | |\  | |___|  _ <
+  |____/|_____|_|/_/   \_\___/|_____|_|   |____/_/   \_\_| \_|_| \_|_____|_| \_\
+
+-->
+
+
+<article id="default-page-banner-section">
+  <div id="default-page-banner">
+    <div class="manic-image-container">
+      <img src="" data-image-desktop="images/about-banner.png">
+    </div>
+  </div>
+</article>
+
+
+
+<!--
+   _____ ___ _____ _     _____
+  |_   _|_ _|_   _| |   | ____|
+    | |  | |  | | | |   |  _|
+    | |  | |  | | | |___| |___
+    |_| |___| |_| |_____|_____|
+
+-->
+
+
+<article id="page-faq-title-section">
+  <div class="container-fluid has-breakpoint">
+    <div class="row">
+      <div class="col-md-12">
+
+        <div id="page-faq-title">
+          <h2>Privacy Policy</h2>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</article>
+
+
+
+<!--
+   _____ _____ ____  __  __ ____     ____ ___  ______   __
+  |_   _| ____|  _ \|  \/  / ___|   / ___/ _ \|  _ \ \ / /
+    | | |  _| | |_) | |\/| \___ \  | |  | | | | |_) \ V /
+    | | | |___|  _ <| |  | |___) | | |__| |_| |  __/ | |
+    |_| |_____|_| \_\_|  |_|____/   \____\___/|_|    |_|
+
+-->
+
+<article id="terms-page-content-section">
+  <div class="container-fluid has-breakpoint">
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-10">
+
+        <div id="terms-page-content-item-container">
+
+          <div class="terms-page-content-item">
+            <div class="default-copy">
+              <h4>Here lies the title of the item</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div class="terms-page-content-item">
+            <div class="default-copy">
+              <h4>Here lies the title of the item</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div class="terms-page-content-item">
+            <div class="default-copy">
+              <h4>Here lies the title of the item</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div class="terms-page-content-item">
+            <div class="default-copy">
+              <h4>Here lies the title of the item</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div> <!-- row -->
+  </div>
+</article>
+
+
+
+<?php include_once('footer.php'); ?>
