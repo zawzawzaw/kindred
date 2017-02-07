2d2b98043b2e462a135e8e5dde8fad9471fbe3f5
shocknawe <jairus.aragon@gmail.com>
2017-02-07 16:27:14 +0800

    Blog Styles

---
+++ blog_individual.php
@@ -0,0 +1,180 @@
+<?php include_once('header.php'); ?>
+
+<!--
+   ____    _    _   _ _   _ _____ ____
+  | __ )  / \  | \ | | \ | | ____|  _ \
+  |  _ \ / _ \ |  \| |  \| |  _| | |_) |
+  | |_) / ___ \| |\  | |\  | |___|  _ <
+  |____/_/   \_\_| \_|_| \_|_____|_| \_\
+
+-->
+
+<article id="blog-page-banner-section">
+  <div id="blog-page-banner">
+    <div class="manic-image-container">
+      <img src="" data-image-desktop="images/blog-banner.jpg">
+    </div>
+    <h1>The Brew Journal</h1>
+  </div>
+</article>
+
+
+
+<!--
+    ____ ___  _   _ _____ _____ _   _ _____
+   / ___/ _ \| \ | |_   _| ____| \ | |_   _|
+  | |  | | | |  \| | | | |  _| |  \| | | |
+  | |__| |_| | |\  | | | | |___| |\  | | |
+   \____\___/|_| \_| |_| |_____|_| \_| |_|
+
+-->
+
+<article id="blog-page-content-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-9">
+
+        <div id="blog-page-content">
+
+          <div id="blog-page-article-container">
+
+
+
+            <article class="blog-page-article">
+
+              <div class="blog-page-article-title">
+                <h1>Teas For Chinese New Year</h1>
+                <h4>january 5, 2017</h4>
+              </div> <!-- blog-page-article-title -->
+
+              <div class="blog-page-article-banner">
+                <div class="manic-image-container">
+                  <img src="" data-image-desktop="images/blog-content-01.jpg">
+                </div>
+              </div> <!-- blog-page-article-banner -->
+
+              <div class="blog-page-article-content">
+                <div class="default-copy">
+                  <p><span class="first-letter">H</span>ello, friends! As we round up 2016, I thought I’d share a warm, delicious drink to ring in the New Year. It’s been a crazy year of ups and downs, and I promise, a sort-of-reflection post will be coming soon as I muse about the past, present, and future of this blog. I never had mulled wine until this October at the Montreal workshop, where we finished our last evening together with rounds of mulled wine. The warm, spiced drink captivated me and I knew this was something that I would make for the holidays. I was gifted two giant (grapefruit-sized) pomegranates, and slowly this combination of spices and pomegranate came to mind.</p>
+                </div>
+
+                <div class="default-copy">
+                  <p><span class="first-letter">H</span>ello, friends! As we round up 2016, I thought I’d share a warm, delicious drink to ring in the New Year. It’s been a crazy year of ups and downs, and I promise, a sort-of-reflection post will be coming soon as I muse about the past, present, and future of this blog. I never had mulled wine until this October at the Montreal workshop, where we finished our last evening together with rounds of mulled wine. The warm, spiced drink captivated me and I knew this was something that I would make for the holidays. I was gifted two giant (grapefruit-sized) pomegranates, and slowly this combination of spices and pomegranate came to mind.</p>
+                </div>
+
+                <div class="default-copy">
+                  <p><span class="first-letter">H</span>ello, friends! As we round up 2016, I thought I’d share a warm, delicious drink to ring in the New Year. It’s been a crazy year of ups and downs, and I promise, a sort-of-reflection post will be coming soon as I muse about the past, present, and future of this blog. I never had mulled wine until this October at the Montreal workshop, where we finished our last evening together with rounds of mulled wine. The warm, spiced drink captivated me and I knew this was something that I would make for the holidays. I was gifted two giant (grapefruit-sized) pomegranates, and slowly this combination of spices and pomegranate came to mind.</p>
+                </div>
+
+
+              </div> <!-- blog-page-article-content -->
+
+            </article> <!-- blog-page-article -->
+
+
+
+            
+          </div> <!-- blog-page-article-container -->
+
+          
+
+        </div> <!-- blog-page-content -->
+
+      </div>
+      <div class="col-md-3">
+
+        <div id="blog-page-sidebar">
+
+          
+          
+          
+
+          <div class="blog-page-sidebar-item" id="blog-page-sidebar-categories">
+            <div class="blog-page-sidebar-item-title">
+              <h4>Categories</h4>
+            </div>
+            <div class="blog-page-sidebar-item-content">
+              <ul>
+                <li><a href="">About Tea</a></li>
+                <li><a href="">All</a></li>
+                <li><a href="">Cafe Guide</a></li>
+                <li><a href="">Featured</a></li>
+                <li><a href="">Food & Drinks</a></li>
+                <li><a href="">Lifestyle</a></li>
+                <li><a href="">Living & Dining</a></li>
+                <li><a href="">News & Events</a></li>
+                <li><a href="">Portraits</a></li>
+                <li><a href="">Tea Facts</a></li>
+                <li><a href="">Tea Health</a></li>
+                <li><a href="">Tea of the Month</a></li>
+              </ul>
+            </div>
+          </div> <!-- #blog-page-sidebar-categories -->
+
+          <div class="blog-page-sidebar-item" id="blog-page-sidebar-tags">
+            <div class="blog-page-sidebar-item-title">
+              <h4>Tags</h4>
+            </div>
+            <div class="blog-page-sidebar-item-content">
+              <ul>
+                <li><a href="">About tea</a></li>
+                <li><a href="">Brewing</a></li>
+                <li><a href="">Artisanal</a></li>
+                <li><a href="">Chinese new year</a></li>
+                <li><a href="">Camomile</a></li>
+                <li><a href="">Cheesecake</a></li>
+                <li><a href="">Chocolate</a></li>
+                <li><a href="">Cafes</a></li>
+              </ul>
+            </div>
+          </div> <!-- #blog-page-sidebar-tags -->
+
+          <div class="blog-page-sidebar-item" id="blog-page-sidebar-popular">
+            <div class="blog-page-sidebar-item-title">
+              <h4>Popular Post</h4>
+            </div>
+            <div class="blog-page-sidebar-item-content">
+                
+              <div class="sidebar-popular-post-item">
+                <h3>Teas for Chinese New Year</h3>
+                <h5>January 5, 2017</h5>
+                <a class="arrow-cta small-version">Learn more</a>
+              </div>
+              
+              <div class="sidebar-popular-post-item">
+                <h3>Teas for Chinese New Year</h3>
+                <h5>January 5, 2017</h5>
+                <a class="arrow-cta small-version">Learn more</a>
+              </div>
+
+              <div class="sidebar-popular-post-item">
+                <h3>Teas for Chinese New Year</h3>
+                <h5>January 5, 2017</h5>
+                <a class="arrow-cta small-version">Learn more</a>
+              </div>
+              
+            </div>
+          </div> <!-- #blog-page-sidebar-popular -->
+
+
+
+
+        </div> <!-- blog-page-sidebar -->
+
+      </div>
+    </div>
+  </div>
+
+  
+
+</article>
+
+
+
+
+
+
+
+
+
+<?php include_once('footer.php'); ?>
