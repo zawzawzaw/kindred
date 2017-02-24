cee3824ff94e3b30dbb71048d1c56f91ab22cd29
zawzawzaw <zawzawzaw@gmail.com>
2017-02-24 11:00:52 +0800

    updated

--- shopify_app/index.php
+++ shopify_app/index.php
@@ -1,7 +1,7 @@
 <?php 
 $api_key = '23d744a08144e3e5c97b876eede866a9';
 $scope = 'write_customers,read_customers';
-$redirect_uri = 'http://clients.manic.com.sg/kindred/shopify_app/callback.php';
+$redirect_uri = 'http://www.manic.com.sg/kindred/shopify_app/callback.php';
 $nonce = uniqid();
 
 session_start();
